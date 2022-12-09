efebc345cce297c9cc6fa05208cc8e714400737e
Bryan Neveu <bryan@pando-studio.com>
2022-12-09 15:26:40 +0100

    UPDATE : Transform file jsx into tsx

---
+++ screens/CreditScreen.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import theme from '../themes/default';
+
+export default function CreditScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.instruction_container}>
+                <Text style={styles.instructions}>Si vous me découvrez, merci de me découvrir !</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.primary0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    instruction_container: {
+        flex: 1,
+        alignItem: 'center',
+        justifyContent: 'center'
+    },
+    instructions: {
+        marginHorizontal: theme.spacing.base,
+        color: theme.colors.white,
+        fontSize: theme.fontSize.lg,
+        textAlign: 'center'
+    }
+});
